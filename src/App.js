94a148f1c9a600efd95e042ec835a1b8a28477c7
Barış ERTUĞRUL <barisertugrul@ertyazilim.com>
2021-06-06 01:29:33 +0300

    11.Gün ders sonu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 import Dashboard from './layouts/Dashboard';
+import { Container } from 'semantic-ui-react';
+import Navi from './layouts/Navi';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard></Dashboard>
+      <Navi/>
+      <Container className="main">
+        <Dashboard></Dashboard>
+      </Container>
+      
     </div>
   );
 }
